9131bef998f40e591b9c95ab04e3f3986d37c600
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-01-26 21:29:20 +0300

    header elements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,30 @@ function Header() {
     <>
       <header>
         <div className="logo__contain">
-          <img src={Logo} alt="" />
+          <img src={Logo} alt="RoboRent" />
         </div>
-        <div className="balance__contain"></div>
-        <div className="user-info__contain"></div>
+        <div className="balance__contain">
+          <p>
+            Баланс: <span>1000</span>Р<a href="#">Пополнить</a>
+          </p>
+          <p>
+            Баланс ботов: <span>1000</span>Р<a href="#">Вывести</a>
+          </p>
+        </div>
+        <div className="user-info__contain">
+          <div className="user-info__inner">
+            <div className="user-info__avatar">
+              <img src="" alt="" />
+            </div>
+            <div className="user-info__links">
+              <p className="user-info__links-name">@Diblaodjsss</p>
+              <p className="user-info__links-telegram">Привязать Telegram</p>
+              <p className="user-info__links-subscription">
+                Подписка: <span>отсутствует</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );
