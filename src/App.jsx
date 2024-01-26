98a03f334ac967ec622282c0991d79e0922e700e
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-01-26 20:45:12 +0300

    header marking

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout.jsx";
+import HomePage from "./components/screens/HomePage/HomePage.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route>
-        <Route></Route>
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
+import "./Header.scss";
+import Logo from "../../assets/svg/logo.svg";
+
 function Header() {
   return (
     <>
-      <header></header>
+      <header>
+        <div className="logo__contain">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="balance__contain"></div>
+        <div className="user-info__contain"></div>
+      </header>
     </>
   );
 }
