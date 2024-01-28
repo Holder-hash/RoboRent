e637c5c8a0cbe4551937f9a01777db3511e77b82
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-01-28 16:28:57 +0300

    aside nav styles

--- src/components/layout/Aside/Aside.jsx
+++ src/components/layout/Aside/Aside.jsx
@@ -3,18 +3,26 @@ import "./Aside.scss";
 function Aside() {
   return (
     <div className="aside">
-      <div className="aside__inner">
-        <p>Прочее:</p>
-        <nav>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </nav>
-      </div>
+      <p>Прочее:</p>
+      <nav>
+        <ul>
+          <li className="nav__item">
+            <a href="">Настройки профиля</a>
+          </li>
+          <li className="nav__item">
+            <a href="">Настроить подписку</a>
+          </li>
+          <li className="nav__item">
+            <a href="">Telegram канал</a>
+          </li>
+          <li className="nav__item">
+            <a href="">Тех. Поддержка</a>
+          </li>
+          <li className="nav__item">
+            <a href="">Все документы и контакты</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
