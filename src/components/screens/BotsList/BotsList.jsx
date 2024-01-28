38c55018a496680ff9712c535eea17de59540582
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-01-28 17:13:29 +0300

    bots list title

--- src/components/screens/BotsList/BotsList.jsx
+++ src/components/screens/BotsList/BotsList.jsx
@@ -3,8 +3,12 @@ import "./BotsList.scss";
 function BotsList() {
   return (
     <>
-      <div className="user-bots"></div>
-      <div className="available-bots"></div>
+      <div className="user-bots">
+        <p className="bots-list__title">Ваши боты:</p>
+      </div>
+      <div className="available-bots">
+        <p className="bots-list__title">Доступные боты к аренде:</p>
+      </div>
     </>
   );
 }
