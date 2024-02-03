ef131fdf713b52c3dbc237b05b1086fc193b4a89
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-02-03 12:43:43 +0300

    rm userBotSinglePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout.jsx";
 import BotsList from "./components/screens/BotsList/BotsList.jsx";
-import UserBotSinglePage from "./components/screens/UserBotSinglePage/UserBotSinglePage.jsx";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/RoboRent" element={<Layout />}>
           <Route index element={<BotsList />} />
-          <Route path="/RoboRent/:id" element={<UserBotSinglePage />} />
         </Route>
       </Routes>
     </>
