772c3a9d19b99f9387d2e0cfa07628ce19248bf0
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-02-05 13:22:13 +0300

    scroll trigger pos change

--- src/components/screens/LandingPage/LandingPage.jsx
+++ src/components/screens/LandingPage/LandingPage.jsx
@@ -26,7 +26,7 @@ function LandingPage() {
       duration: 0.7,
       scrollTrigger: {
         trigger: ".other",
-        start: "top 95%",
+        start: "top 90%",
       },
     });
     gsap.to(".rates", {
@@ -36,7 +36,7 @@ function LandingPage() {
 
       scrollTrigger: {
         trigger: ".rates",
-        start: "top 95%",
+        start: "top 90%",
       },
     });
   }, []);
